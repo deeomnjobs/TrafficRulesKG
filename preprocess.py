9d671052897d0d57aaefaf0c4160e4997354738c
Deeomn <2206247007@qq.com>
2023-03-29 21:16:49 +0800

    label

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
 import json
 
+label = ['B-ACT', 'I-ACT', 'B-LAW', 'I-LAW', 'B-SCORE', 'I-SCORE', 'B-PUNISH', 'I-PUNISH']
+
 
 def preprocess_file():
     violation_list = []
     json_list = []
-    with open("./resources/violation.json", "r",encoding='utf-8') as f:
+    with open("./resources/violation.json", "r", encoding='utf-8') as f:
         for line in f.readlines():
             violation_list.append(json.loads(line.replace("/n", "")))
