e7892e64859a166d6917dc1c5990425449a0e12c
Deeomn <2206247007@qq.com>
2023-05-17 09:12:17 +0800

    pyneo

--- format_convert.py
+++ format_convert.py
@@ -1,16 +1,72 @@
 # coding:utf-8
-set_act  = set()
-removal_list = []
-def extract(sentence,start):
+import csv
+import json
 
-    print()
-def w2ner2(list_predict):
-    # predict [0] sentence [1] ACT tuple [2] SCORE tuple [3] PUNISH tuple [4] LAW tuple
+
+def w2ner2list(list_predict):
+    act_set = set()
+    nodes_list = []
+    temp = {}
+    # [(
+    # 号牌为沪AAP8***的小型轿车在逸仙高架东侧近场中路下匝道路段，实施“机动车违反禁止标线指示的”违法行为，处罚：罚款200元，记1分。\n',
+    # ('罚款200元', 'PUNISH', 74, 79),
+    # ('记1分', 'SCORE', 81, 83),
+    # ('机动车违反禁止标线指示的', 'ACT', 53, 64)
+    # )
+    # ,()
+    # ]
     for predict in list_predict:
-        if predict[1][1] not in set_act:
-            set_act.add(predict[1][1])
-            temp = {}
-            for data in predict[1:]:
-                temp[data[1]] = data[0]
-            removal_list.append(temp)
-    return removal_list
+        for entity in predict[1:]:
+            if entity[1] == 'ACT':
+                if entity[0] not in act_set:
+                    act_set.add(entity[0])
+                    for e in predict[1:]:
+                        temp[e[1]] = e[0]
+                    nodes_list.append(temp)
+                    temp = {}
+    return nodes_list
+
+
+def w2ner_write2csv(nodes_list):
+    with open('ShangHai.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['ACT', 'PUNISH', 'SCORE', 'BELONG'])
+        for node in nodes_list:
+            act = '####'
+            punish = '####'
+            score = '####'
+            belong = '上海市地方法规'
+            if 'ACT' in node:
+                act = node['ACT']
+            if 'PUNISH' in node:
+                punish = node['PUNISH']
+            if 'SCORE' in node:
+                score = node['SCORE']
+            writer.writerow([act, punish, score, belong])
+
+def nation_writer2csv(struct_list):
+    with open('./nation.csv','w',encoding='utf-8',newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['ACT', 'PUNISH', 'SCORE', 'BELONG'])
+        for node in struct_list:
+            act = '####'
+            punish = '####'
+            score = '####'
+            belong = '上海市地方法规'
+            if 'ACT' in node:
+                act = node['ACT']
+            if 'PUNISH' in node:
+                punish = node['PUNISH']
+            if 'SCORE' in node:
+                score = node['SCORE']
+            writer.writerow([act, punish, score, belong])
+# with open('./resources/output.txt', 'r', encoding='GBK') as f:
+#     content = f.read()
+# content = eval(content)
+# unstruct_nodes_list = w2ner2list(content)
+# write2csv(unstruct_nodes_list)
+
+struct_list = []
+with open("./resources/struct_res.json","r",encoding='utf-8') as f:
+        struct_list = json.load(f)
+
--- pyneo.py
+++ pyneo.py
@@ -1,74 +1,308 @@
+import json
+
 from neo4j import GraphDatabase
 import format_convert
+
 # 连接neo4j
 uri = "neo4j://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4jneo4j"))
 
 
+def init_node(tx):
+    tx.run("MERGE (n1:OtherNode{name:'交通法规'})"
+           "MERGE (n2:OtherNode{name:'地方法规'})"
+           "MERGE (n3:OtherNode{name:'全国性法规'})"
+           "MERGE (n4:OtherNode{name:'武汉市地方法规'})"
+           "MERGE (n5:OtherNode{name:'江永县地方法规'})"
+           "MERGE (n6:OtherNode{name:'上海市地方法规'})"
+           )
+
+def init_relationships(tx):
+    tx.run("MATCH (n1:OtherNode{name:'交通法规'}),(n2:OtherNode{name:'地方法规'})"
+           "MERGE (n2)-[:属于]->(n1)"
+           )
+    tx.run("MATCH (n1:OtherNode{name:'交通法规'}),(n2:OtherNode{name:'全国性法规'})"
+           "MERGE (n2)-[:属于]->(n1)"
+           )
+    tx.run("MATCH (n1:OtherNode{name:'地方法规'}),(n2:OtherNode{name:'江永县地方法规'})"
+           "MERGE (n2)-[:属于]->(n1)"
+           )
+    tx.run("MATCH (n1:OtherNode{name:'地方法规'}),(n2:OtherNode{name:'武汉市地方法规'})"
+           "MERGE (n2)-[:属于]->(n1)"
+           )
+    tx.run("MATCH (n1:OtherNode{name:'地方法规'}),(n2:OtherNode{name:'上海市地方法规'})"
+           "MERGE (n2)-[:属于]->(n1)"
+           )
+
+def init_act_rule(tx):
+    tx.run("match (from:ACT{belong:'上海市地方法规'}),(to:OtherNode{name:'上海市地方法规'})"
+           "merge (from)-[:属于]->(to)"
+           )
+    tx.run("match (from:ACT{belong:'武汉市地方法规'}),(to:OtherNode{name:'武汉市地方法规'})"
+           "merge (from)-[:属于]->(to)"
+           )
+    tx.run("match (from:ACT{belong:'全国性法规'}),(to:OtherNode{name:'全国性法规'})"
+           "merge (from)-[:属于]->(to)"
+           )
+    tx.run("match (from:ACT{belong:'江永县地方法规'}),(to:OtherNode{name:'江永县地方法规'})"
+           "merge (from)-[:属于]->(to)"
+           )
+
 # 创建节点(ACT)
-def merge_act(tx, name):
-    tx.run("MERGE (a:ACT {name: $name})", name=name)
+def merge_act(tx, name, belong):
+    result = tx.run("MERGE (a:ACT {name: $name,belong:$belong})", name=name, belong=belong)
+    return result
 
 
 # 创建节点(LAW)
 def merge_law(tx, name):
-    tx.run("CREATE (a:LAW {name: $name})", name=name)
+    result = tx.run("MERGE (a:LAW {name: $name})", name=name)
+    return result
 
 
 # 创建节点(PUNISH)
 def merge_punish(tx, name):
-    tx.run("CREATE (a:PUNISH {name: $name})", name=name)
+    result = tx.run("MERGE (a:PUNISH {name: $name})", name=name)
+    return result
 
 
 # 创建节点(SCORE)
 def merge_score(tx, name):
-    tx.run("CREATE (a:SCORE {name: $name})", name=name)
+    result = tx.run("MERGE (a:SCORE {name: $name})", name=name)
+    return result
 
 
 # 定义创建节点关系(ACT according to LAW)
-def merge_relationship_according_to(tx, ACT_name, LAW_name):
-    tx.run("MATCH (act:ACT{name:$ACT_name}),(law:LAW{name:$LAW_name})"
+def merge_relationship_according_to(tx, ACT_name, LAW_name, ACT_belong):
+    tx.run("MATCH (act:ACT{name:$ACT_name,belong:$ACT_belong}),(law:LAW{name:$LAW_name})"
            "MERGE (act)-[:根据]->(law)",
-           ACT_name=ACT_name, LAW_name=LAW_name)
+           ACT_name=ACT_name, LAW_name=LAW_name, ACT_belong=ACT_belong)
 
 
 # 定义创建节点关系(ACT punishment PUNISH)
-def merge_relationship_punishment_is(tx, ACT_name, PUNISH_name):
-    tx.run("MATCH (act:ACT{name:$ACT_name}),(punish:PUNISH{name:$PUNISH_name})"
+def merge_relationship_punishment_is(tx, ACT_name, PUNISH_name, ACT_belong):
+    tx.run("MATCH (act:ACT{name:$ACT_name,belong:$ACT_belong}),(punish:PUNISH{name:$PUNISH_name})"
            "MERGE (act)-[:惩罚措施]->(punish)",
-           ACT_name=ACT_name, PUNISH_name=PUNISH_name)
+           ACT_name=ACT_name, PUNISH_name=PUNISH_name, ACT_belong=ACT_belong)
 
 
 # 定义创建节点关系(ACT deduction_of_points SCORE)
-def merge_relationship_deduction_of_points(tx, ACT_name, SCORE_name):
-    tx.run("MATCH (act:ACT{name:$ACT_name}),(score:SCORE{name:SCORE_name})"
+def merge_relationship_deduction_of_points(tx, ACT_name, SCORE_name, ACT_belong):
+    tx.run("MATCH (act:ACT{name:$ACT_name,belong:$ACT_belong}),(score:SCORE{name:$SCORE_name})"
            "MERGE (act)-[:记分]->(score)",
-           ACT_name=ACT_name, SCORE_name=SCORE_name)
+           ACT_name=ACT_name, SCORE_name=SCORE_name, ACT_belong=ACT_belong)
+
+# 批处理创建节点(上海)
+def batch_create_nodes_ShangHai(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///ShangHai.csv' AS line   "
+           "MERGE (n1:ACT{name:line.ACT,belong:line.BELONG})"
+           "MERGE (n2:PUNISH{name:line.PUNISH})"
+           "MERGE (n3:SCORE{name:line.SCORE})"
+           )
+
+# 批处理创建节点(全国性)
+def batch_create_nodes_Nation(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///Nation.csv' AS line    "
+           "MERGE (n1:ACT{name:line.ACT,belong:line.BELONG,code:line.CODE})"
+           "MERGE (n2:PUNISH{name:line.PUNISH})"
+           "MERGE (n3:SCORE{name:line.SCORE})"
+           "MERGE (n4:OTHER_PENALTIES{name:line.OTHER_PENALTIES})"
+           )
+
+# 批处理创建节点(武汉)
+def batch_create_nodes_WuHan(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "MERGE (n1:ACT{name:line.ACT,belong:line.BELONG,code:line.CODE})"
+           "MERGE (n2:PUNISH{name:line.PUNISH})"
+           "MERGE (n3:SCORE{name:line.SCORE})"
+           "MERGE (n4:LAW{name:line.LAW})"
+           "MERGE (n5:PUNISH_BASIS{name:line.PUNISH_BASIS})"
+           "MERGE (n6:ENFORCEMENT{name:line.ENFORCEMENT})"
+           "MERGE (n7:ENFORCEMENT_BASIS{name:line.ENFORCEMENT_BASIS})"
+           "MERGE (n8:OTHER_MEASURES{name:line.OTHER_MEASURES})"
+           "MERGE (n9:BELONG{name:line.BELONG})"
+           )
+
+# 批处理创建节点(江永)
+def batch_create_nodes_JiangYong(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line    "
+           "MERGE (n1:ACT{name:line.ACT,belong:line.BELONG,code:line.CODE})"
+           "MERGE (n2:PUNISH{name:line.PUNISH})"
+           "MERGE (n3:SCORE{name:line.SCORE})"
+           "MERGE (n4:LAW{name:line.LAW})"
+           "MERGE (n5:PUNISH_BASIS{name:line.PUNISH_BASIS})"
+           "MERGE (n6:ENFORCEMENT{name:line.ENFORCEMENT})"
+           "MERGE (n7:ENFORCEMENT_BASIS{name:line.ENFORCEMENT_BASIS})"
+           )
 
 
-def create_nodes(nodes_list):
+def batch_create_relationships_Shanghai(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///ShangHai.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:PUNISH{name:line.PUNISH})"
+           "merge (from)-[r:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///ShangHai.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:SCORE{name:line.SCORE})"
+           "merge (from)-[r:记分]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///ShangHai.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:PUNISH{name:line.PUNISH})"
+           "merge (from)-[r:惩罚措施]->(to)"
+           )
+
+def batch_create_relationships_WuHan(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:PUNISH{name:line.PUNISH})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:OTHER_PENALTIES{name:line.OTHER_PENALTIES})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:OTHER_MEASURES{name:line.OTHER_MEASURES})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:ENFORCEMENT{name:line.ENFORCEMENT})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:SCORE{name:line.SCORE})"
+           "merge (from)-[:记分]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:LAW{name:line.LAW})"
+           "merge (from)-[:依据]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:PUNISH{name:line.PUNISH}),(to:PUNISH_BASIS{name:line.PUNISH_BASIS})"
+           "merge (from)-[:依据]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:ENFORCEMENT{name:line.ENFORCEMENT}),(to:ENFORCEMENT_BASIS{name:line.ENFORCEMENT_BASIS})"
+           "merge (from)-[:依据]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///WuHan.csv' AS line   "
+           "match (from:OTHER_MEASURES{name:line.OTHER_MEASURES}),(to:PUNISH_BASIS{name:line.OTHER_MEASURES_BASIS})"
+           "merge (from)-[:依据]->(to)"
+           )
+
+def batch_create_relationships_Nation(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///Nation.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:PUNISH{name:line.PUNISH})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///Nation.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:OTHER_PENALTIES{name:line.OTHER_PENALTIES})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///Nation.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:SCORE{name:line.SCORE})"
+           "merge (from)-[:记分]->(to)"
+           )
+
+def batch_create_relationships_JiangYong(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:PUNISH{name:line.PUNISH})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:ENFORCEMENT{name:line.ENFORCEMENT})"
+           "merge (from)-[:惩罚措施]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:SCORE{name:line.SCORE})"
+           "merge (from)-[:记分]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line   "
+           "match (from:ACT{name:line.ACT,belong:line.BELONG}),(to:LAW{name:line.LAW})"
+           "merge (from)-[:依据]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line   "
+           "match (from:PUNISH{name:line.PUNISH}),(to:PUNISH_BASIS{name:line.PUNISH_BASIS})"
+           "merge (from)-[:依据]->(to)"
+           )
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///JiangYong.csv' AS line   "
+           "match (from:ENFORCEMENT{name:line.ENFORCEMENT}),(to:ENFORCEMENT_BASIS{name:line.ENFORCEMENT_BASIS})"
+           "merge (from)-[:依据]->(to)"
+           )
+
+def create_nodes(nodes_list, belong):
+    i = 0
     for node in nodes_list:
         with driver.session() as session:
-            if node.has_key('ACT'):
-                session.execute_write(merge_act, node.get('ACT'))
-            if node.has_key('LAW'):
+            print(i)
+            if 'ACT' in node:
+                session.execute_write(merge_act, node.get('ACT'), belong)
+            if 'LAW' in node:
                 session.execute_write(merge_law, node.get('LAW'))
-            if node.has_key('PUNISH'):
+            if 'PUNISH' in node:
                 session.execute_write(merge_punish, node.get('PUNISH'))
-            if node.has_key('SCORE'):
+            if 'SCORE' in node:
                 session.execute_write(merge_score, node.get('SCORE'))
+            session.close()
+            i = i + 1
 
-def create_relationship(nodes_list):
+
+def create_relationship(nodes_list, belong):
+    i = 0
     for node in nodes_list:
         with driver.session() as session:
-            if node.has_key('ACT') and node.has_key('LAW'):
-                session.execute_write(merge_relationship_according_to, node.get('ACT'), node.get('LAW'))
-            if node.has_key('ACT') and node.has_key('PUNISH'):
-                session.execute_write(merge_relationship_punishment_is, node.get('ACT'), node.get('PUNISH'))
-            if node.has_key('ACT') and node.has_key('SCORE'):
-                session.execute_write(merge_relationship_deduction_of_points, node.get('ACT'), node.get('SCORE'))
+            print(i)
+            if 'ACT' in node and 'LAW' in node:
+                res = session.execute_write(merge_relationship_according_to, node.get('ACT'), node.get('LAW'), belong)
+            if 'ACT' in node and 'PUNISH' in node:
+                res = session.execute_write(merge_relationship_punishment_is, node.get('ACT'), node.get('PUNISH'),
+                                            belong)
+            if 'ACT' in node and 'SCORE' in node:
+                res = session.execute_write(merge_relationship_deduction_of_points, node.get('ACT'), node.get('SCORE'),
+                                            belong)
+            print(res)
+            session.close()
+        i = i + 1
+
+def removal(tx):
+    tx.run("LOAD CSV WITH HEADERS  FROM 'file:///Nation.csv' AS line   "
+           "match (node1:Act{name:line.ACT})"
+           "where node1.belong <> '全国性法规'"
+           "delete node1"
+    )
+
 
 if __name__ == '__main__':
-    list_predict = []
-    create_nodes(format_convert.w2ner2(list_predict))
-    create_relationship(format_convert.w2ner2(list_predict))
+    # # 结构化数据
+    # list_struct_data = []
+    # with open('./resources/struct_res.json', 'r', encoding='utf-8') as f:
+    #     list_struct_data = json.load(f)
+    # struct_nodes_list = list_struct_data
+    # # 非机构化数据
+    # with open('./resources/output.txt', 'r', encoding='GBK') as f:
+    #     content = f.read()
+    # content = eval(content)
+    # unstruct_nodes_list = format_convert.w2ner2list(content)
+    #
+    # create_nodes(struct_nodes_list)
+    # create_nodes(unstruct_nodes_list, "上海市地方法规")
+    # create_relationship(struct_nodes_list)
+    # create_relationship(unstruct_nodes_list, "上海市地方法规")
+    # driver.close()
+    with driver.session() as session:
+        # session.execute_write(init_node)
+        # session.execute_write(init_relationships)
+        # # 上海
+        # session.execute_write(batch_create_nodes_ShangHai)
+        # session.execute_write(batch_create_relationships_Shanghai)
+        # # 武汉
+        # session.execute_write(batch_create_nodes_WuHan)
+        # session.execute_write(batch_create_relationships_WuHan)
+        # # 江永
+        # session.execute_write(batch_create_nodes_JiangYong)
+        # session.execute_write(batch_create_relationships_JiangYong)
+        # 全国性
+        # session.execute_write(batch_create_nodes_Nation)
+        # session.execute_write(batch_create_relationships_Nation)
+        #
+        # session.execute_write(init_act_rule)
+        session.execute_write(removal)
+    driver.close()
